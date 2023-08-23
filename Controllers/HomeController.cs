38f9d946e304d0550eb5a5c12563da8ccd957954
Leonardo Barozzi <leonardo.barozzi@mail.com>
2023-08-23 18:15:28 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimplyDebug.Models;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace SimplyDebug.Controllers {
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+            ViewBag.Request = Request;
+            /*
+            foreach(KeyValuePair<string,string> tk in Request.Query) {
+                tk.
+            }
+            //*/
+            return View();
+        }
+        [HttpPost("/")]
+        public IActionResult IndexPost() {
+            ViewBag.Request = Request;
+            /*
+            foreach(KeyValuePair<string,string> tk in Request.Query) {
+                tk.
+            }
+            //*/
+            return View("/Views/Home/Index.cshtml");
+        }
+        //*
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace SimplyDebug.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
